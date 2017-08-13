1586ab376b2e3ccfdb4fb0eef9fa558a7f6c3b2c
braid123 <603505420@qq.com>
2017-08-13 23:06:19 +0800

    init commit

---
+++ Python/13Roman_To_Integer.py
@@ -0,0 +1,21 @@
+"""
+Given a roman numeral, convert it to an integer.
+Input is guaranteed to be within the range from 1 to 3999.
+"""
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        规则一：最后一位字母肯定是加除了最后一位
+        规则二：若一位字母比下一位小，则减
+        """
+        roman = {'M': 1000,'D': 500 ,'C': 100,'L': 50,'X': 10,'V': 5,'I': 1}
+        z = 0
+        for i in range(0, len(s) - 1):
+            if roman[s[i]] < roman[s[i+1]]:
+                z -= roman[s[i]]
+            else:
+                z += roman[s[i]]
+        return z + roman[s[-1]]
